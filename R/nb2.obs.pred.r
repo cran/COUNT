83577895616b6cd2f3ed10970d48da7313299d1c
Joseph M Hilbe <hilbe@asu.edu>
2010-10-06 00:00:00 +0000

    version 1.1.1

--- R/nb2.obs.pred.r
+++ R/nb2.obs.pred.r
@@ -4,16 +4,20 @@
 
 nb2.obs.pred <- function(len, model)  {
   mu <- fitted(model)
+  trun.y <- model$y[model$y < len+1]
+  obs <- as.data.frame(table(trun.y) / length(model$y) * 100)
+  names(obs) <- c("Count","propObsv")
   alpha <- 1/model$theta
   amu <- alpha*mu
-  avgp <- sapply(0:len, function(i)
-     mean( exp(i*log(amu/(1+amu)) - (1/alpha)*log(1+amu)  + 
-     log( gamma(i + 1/alpha) ) - log( gamma(i + 1) ) - 
-     log( gamma(1 / alpha)))))
-  trun.y <- model$y[model$y < len+1]
-  propObsv <- table(trun.y) / length(trun.y)
-  Diff <- c(0,propObsv)*100 - avgp[1:(len+1)]*100
-  data.frame(LOS=0:len, ObsProp=c(0,propObsv)*100, avgp*100, Diff)
+  pred <- data.frame(Count = 0:len,
+                     propPred = sapply(0:len, function(i)
+                       mean(exp(i*log(amu/(1+amu)) - (1/alpha) * log(1+amu)  + 
+                                log(gamma(i + 1/alpha) ) - log(gamma(i + 1) ) - 
+                                log(gamma(1 / alpha))))) * 100)
+  out <- merge(pred, obs, all=TRUE)
+  out$propObsv[is.na(out$propObsv)] <- 0
+  out$Diff <- with(out, propObsv - propPred)
+  return(out[,c(1,3,2,4)])
 }
 
 # nb2.obs.pred(len=25, model=mdpar)
--- R/poi.obs.pred.r
+++ R/poi.obs.pred.r
@@ -1,18 +1,29 @@
-# library(COUNT)
-# data(medpar)
-# mdpar <- glm(los ~ hmo+white+type2+type3, family=poisson, data=medpar, y=TRUE, model=TRUE)
-
 # Create a table of observed vs predicted Poisson counts, and difference
 #  following glm()  :  see poi.obs.pred.Rd for usage
 # See Hilbe, J.M (2011), Negative Binomial Regression, 2nd ed, Cambridge Univ Press
+
+
 poi.obs.pred <- function(len, model)  {
   mu <- fitted(model)
-  avgp <- sapply(0:len, function(i)
-                        mean(exp(-mu)*(mu^i)/factorial(i)))
   trun.y <- model$y[model$y < len+1]
-  propObsv <- table(trun.y) / length(trun.y)
-  Diff <- c(0,propObsv)*100 - avgp[1:(len+1)]*100
-  data.frame(LOS=0:len, ObsProp=c(0,propObsv)*100, avgp*100, Diff)
+  obs <- as.data.frame(table(trun.y) / length(model$y) * 100)
+  names(obs) <- c("Count","propObsv")
+  pred <- data.frame(Count = 0:len,
+                     propPred = sapply(0:len, function(i)
+                       mean(exp(-mu)*(mu^i)/factorial(i))) * 100)
+  out <- merge(pred, obs, all=TRUE)
+  out$propObsv[is.na(out$propObsv)] <- 0
+  out$Diff <- with(out, propObsv - propPred[1:(len+1)])
+  return(out[,c(1,3,2,4)])
 }
 
-# poi.obs.pred(len=25, model=mdpar)
+#library(COUNT)
+#data(medpar)
+#
+#mdpar <- glm(los ~ hmo+white+type2+type3, family=poisson, data=medpar,
+#             y=TRUE, model=TRUE)
+#poi.obs.pred(len=25, model=mdpar)
+#
+#data(affairs)
+#myglm <- glm(naffairs ~ kids, family=poisson, data=affairs, y=TRUE, model=TRUE)
+#poi.obs.pred(len=8, model=myglm)
